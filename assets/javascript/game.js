0588208be4edf32c40b5402f55322c3b9740487f
Tyler Schmauch <tylers@kornerstoneadmin.com>
2018-02-14 16:39:42 -0700

    DOM updates in update area, wins and HP

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -36,21 +36,23 @@
 
 $(document).ready(function () {
 	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	var frodo = { name: 'Frodo', hp: 80, attackPower: 5, attackGrow: 10, counterPower: 10, loot: '<img class="loot" src="assets/images/the-one-ring.png" alt="Ring">', display: '<img class="heroimg" src="assets/images/frodo.jpg_c200" alt="Frodo"><div class="text-block"><h5>Frodo</h5></div><div class="hp"><h5>80</h5></div>' };
-	var gollum = { name: 'Gollum', hp: 120, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/deadfish.jpg_c200" alt="Fish">', display: '<img class="heroimg" src="assets/images/gollum.png" alt="Gollum"><div class="text-block"><h5>Gollum</h5></div><div class="hp">	<h5>120</h5></div>' };
-	var gandalf = { name: 'Gandalf', hp: 150, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/witch-hat.jpg" alt="Wizard Hat">', display: '<img class="heroimg" src="assets/images/gandalf.jpg" alt="Gandalf"><div class="text-block">	<h5>Gandalf</h5></div><div class="hp"><h5>150</h5></div>' };
-	var sauron = { name: 'Sauron', hp: 200, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/EyeSauron.jpg" alt="Eye">', display: '<img class="heroimg" src="assets/images/sauron.jpg" alt="Sauron"><div class="text-block"><h5>Sauron</h5></div><div class="hp"><h5>200</h5></div>' };
+	var frodo = { name: 'Frodo', hp: 80, attackPower: 5, attackGrow: 10, counterPower: 10, loot: '<img class="loot" src="assets/images/the-one-ring.png" alt="Ring">', display: '<img class="heroimg" src="assets/images/frodo.jpg_c200" alt="Frodo"><div class="text-block"><h5>Frodo</h5></div><div class="hp" id="hp1"><h5>80</h5></div>' };
+	var gollum = { name: 'Gollum', hp: 120, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/deadfish.jpg_c200" alt="Fish">', display: '<img class="heroimg" src="assets/images/gollum.png" alt="Gollum"><div class="text-block"><h5>Gollum</h5></div><div class="hp" id="hp2">	<h5>120</h5></div>' };
+	 var gandalf = { name: 'Gandalf', hp: 150, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/witch-hat.jpg" alt="Wizard Hat">', display: '<img class="heroimg" src="assets/images/gandalf.jpg" alt="Gandalf"><div class="text-block">	<h5>Gandalf</h5></div><div class="hp" id="hp3"><h5>150</h5></div>' };
+	var sauron = { name: 'Sauron', hp: 200, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/EyeSauron.jpg" alt="Eye">', display: '<img class="heroimg" src="assets/images/sauron.jpg" alt="Sauron"><div class="text-block"><h5>Sauron</h5></div><div class="hp" id="hp4"><h5>200</h5></div>'};
 	var charBool = false;
 	var enemyBool = false;
 	var myHero;
 	var myEnemy;
 	var characterArray = [frodo,gollum,gandalf,sauron];
+	var winCounter = 0;
+	var lossCounter = 0;
 
 	function initializeGame() {
-		frodo = { name: 'Frodo', hp: 80, attackPower: 5, attackGrow: 10, counterPower: 10, loot: '<img class="loot" src="assets/images/the-one-ring.png" alt="Ring">', display: '<img class="heroimg" src="assets/images/frodo.jpg_c200" alt="Frodo"><div class="text-block"><h5>Frodo</h5></div><div class="hp"><h5>80</h5></div>' };
-		gollum = { name: 'Gollum', hp: 120, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/deadfish.jpg_c200" alt="Fish">', display: '<img class="heroimg" src="assets/images/gollum.png" alt="Gollum"><div class="text-block"><h5>Gollum</h5></div><div class="hp">	<h5>120</h5></div>' };
-		gandalf = { name: 'Gandalf', hp: 150, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/witch-hat.jpg" alt="Wizard Hat">', display: '<img class="heroimg" src="assets/images/gandalf.jpg" alt="Gandalf"><div class="text-block">	<h5>Gandalf</h5></div><div class="hp"><h5>150</h5></div>' };
-		sauron = { name: 'Sauron', hp: 200, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/EyeSauron.jpg" alt="Eye">', display: '<img class="heroimg" src="assets/images/sauron.jpg" alt="Sauron"><div class="text-block"><h5>Sauron</h5></div><div class="hp"><h5>200</h5></div>'};
+		frodo = { name: 'Frodo', hp: 80, attackPower: 5, attackGrow: 10, counterPower: 10, loot: '<img class="loot" src="assets/images/the-one-ring.png" alt="Ring">', display: '<img class="heroimg" src="assets/images/frodo.jpg_c200" alt="Frodo"><div class="text-block"><h5>Frodo</h5></div><div class="hp" id="hp1"><h5>80</h5></div>' };
+		gollum = { name: 'Gollum', hp: 120, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/deadfish.jpg_c200" alt="Fish">', display: '<img class="heroimg" src="assets/images/gollum.png" alt="Gollum"><div class="text-block"><h5>Gollum</h5></div><div class="hp" id="hp2">	<h5>120</h5></div>' };
+		gandalf = { name: 'Gandalf', hp: 150, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/witch-hat.jpg" alt="Wizard Hat">', display: '<img class="heroimg" src="assets/images/gandalf.jpg" alt="Gandalf"><div class="text-block">	<h5>Gandalf</h5></div><div class="hp" id="hp3"><h5>150</h5></div>' };
+		sauron = { name: 'Sauron', hp: 200, attackPower: 5, attackGrow: 5, counterPower: 10, loot: '<img class="loot" src="assets/images/EyeSauron.jpg" alt="Eye">', display: '<img class="heroimg" src="assets/images/sauron.jpg" alt="Sauron"><div class="text-block"><h5>Sauron</h5></div><div class="hp" id="hp4"><h5>200</h5></div>'};
 		$('#hero1').html(frodo.display);
 		$('#hero2').html(gollum.display);
 		$('#hero3').html(gandalf.display);
@@ -59,6 +61,7 @@ $(document).ready(function () {
 		enemyBool = false;
 		myHero = '';
 		myEnemy = '';
+		$('#score').text('Wins: ' + winCounter + '   Losses: ' + lossCounter);
 		// $('.enemy').attr("class","col-md-2");
 	}
 
@@ -75,18 +78,22 @@ $(document).ready(function () {
 				case hero1:
 					myHero = frodo;
 					characterArray.splice(0, 1);
+					$('#hp1').attr('id', 'heroHP');
 					break;
 				case hero2:
 					myHero = gollum;
 					characterArray.splice(1, 1);
+					$('#hp2').attr('id', 'heroHP');
 					break;
 				case hero3:
 					myHero = gandalf;
 					characterArray.splice(2, 1);
+					$('#hp3').attr('id', 'heroHP');
 					break;
 				case hero4:
 					myHero = sauron;
 					characterArray.splice(3, 1);
+					$('#hp4').attr('id', 'heroHP');
 					break;
 			}
 			console.log(characterArray);
@@ -102,21 +109,25 @@ $(document).ready(function () {
 					myEnemy = frodo;
 					var indexFrodo = characterArray.indexOf(frodo);
 					characterArray.splice(indexFrodo, 1);
+					$('#hp1').attr('id', 'enemyHP');
 					break;
 				case hero2:
 					myEnemy = gollum;
 					var indexGollum = characterArray.indexOf(frodo);
 					characterArray.splice(indexGollum, 1);
+					$('#hp2').attr('id', 'enemyHP');
 					break;
 				case hero3:
 					myEnemy = gandalf;
 					var indexGandalf = characterArray.indexOf(frodo);
 					characterArray.splice(indexGandalf, 1);
+					$('#hp3').attr('id', 'enemyHP');
 					break;
 				case hero4:
 					myEnemy = sauron;
 					var indexSauron = characterArray.indexOf(frodo);
 					characterArray.splice(indexSauron, 1);
+					$('#hp4').attr('id', 'enemyHP');
 					break;
 			}
 			console.log(characterArray);
@@ -129,11 +140,13 @@ $(document).ready(function () {
 		if (characterArray.length !== 0 && myHero.hp > 0) {
 		console.log(myHero);
 		myHero.hp = myHero.hp - myEnemy.counterPower;
-		// $('.hp').text(myHero.hp);
+		$('#heroHP').text(myHero.hp);
 		myEnemy.hp = myEnemy.hp - myHero.attackPower;
-		console.log(myEnemy.hp);
+		$('#enemyHP').text(myEnemy.hp);
 		myHero.attackPower = myHero.attackPower + myHero.attackGrow;
 		console.log(myHero.attackPower);
+		$('#updateArea').text('You\'ve dealt ' + myHero.attackPower + ' damage to ' + myEnemy.name + '! ' + myEnemy.name + ' dealt ' + myEnemy.counterPower + ' damage to you.');
+	
 		if (myEnemy.hp <= 0) {
 			$('#updateArea').text('You\'ve slain ' + myEnemy.name + '!');
 			$('.enemy').hide();
@@ -144,10 +157,12 @@ $(document).ready(function () {
 
 	}
 	else if (characterArray.length !== 0 && myHero.hp <= 0) {
+		lossCounter ++;
 		alert('You lose!');
 		initializeGame();
 	}
 	else {
+		winCounter++;
 		alert('You win!');
 		initializeGame();
 	};
